dcac14092fb979ab704b7dc94588adb62d4c18bf
masalskyi <masalskyi@stud.onu.edu.ua>
2022-07-25 06:10:28 +0300

    saved memory

--- maslourl/models/maslou_rl.py
+++ maslourl/models/maslou_rl.py
@@ -10,21 +10,24 @@ import numpy as np
 from maslourl.models.replay_buffer import ReplayBuffer
 from maslourl.trackers.file_logger import FileLogger
 from maslourl.trackers.average_tracker import AverageRewardTracker
-from maslourl.models.utils import get_q_values, select_action_epsilon_greedy
+from maslourl.models.utils import get_q_values, select_action_epsilon_greedy, select_best_action
 
 
 # performs double q learning for discrete action space in episode MDP
 class MaslouRLModel2QDiscrete(ABC):
-    def __init__(self, env, replay_buffer_size=10000):
+    def __init__(self, env, replay_buffer_size=10000, train_state=True, need_to_initiate_memory=True):
         self.env = env
         self.input_shape = env.observation_space.shape
         self.n_actions = env.action_space.n
-        self.action_space = np.array([i for i in range(self.n_actions)])
-        self.memory = ReplayBuffer(replay_buffer_size, input_shape=self.input_shape, n_actions=self.n_actions)
+        self.train_state = train_state
+        self.memory = None
+        if self.train_state and need_to_initiate_memory:
+            self.memory = ReplayBuffer(replay_buffer_size, input_shape=self.input_shape, n_actions=self.n_actions)
 
         self.Q_eval = self.build_model()
-        self.Q_target = self.build_model()
-        self.update_target_model()
+        if self.train_state:
+            self.Q_target = self.build_model()
+            self.update_target_model()
         self.epsilon = 0
 
     def summary(self):
@@ -37,6 +40,8 @@ class MaslouRLModel2QDiscrete(ABC):
               training_batch_size=128, discount_factor=0.99,
               model_backup_frequency_episodes=100, path_to_back_up="./",
               episodes_for_average_tracking=100, file_logger=None):
+        if not self.train_state:
+            raise ValueError("Agent is not in train state")
         self.memory.clear()
         average_tracker = AverageRewardTracker(episodes_for_average_tracking)
         self.epsilon = starting_epsilon
@@ -64,7 +69,7 @@ class MaslouRLModel2QDiscrete(ABC):
                 f"Average reward over last {episodes_for_average_tracking}: {average:.2f} "
                 f"And took: {(time() - episode_start_time):.2f} seconds. ")
             if file_logger is not None:
-                file_logger.log(episode, step+1, episode_reward, average, self.epsilon)
+                file_logger.log(episode, step + 1, episode_reward, average, self.epsilon)
             if episode != 0 and episode % model_backup_frequency_episodes == 0:
                 backup_file = path_to_back_up + f"model_{episode}.h5"
                 print(f"Backing up model to {backup_file}")
@@ -72,6 +77,8 @@ class MaslouRLModel2QDiscrete(ABC):
         self.epsilon = 0
 
     def test(self, episodes, max_steps_per_episode, visualize=False):
+        if self.train_state:
+            raise ValueError("Model is in train state")
         rewards = []
         self.epsilon = 0
         for episode in range(episodes):
@@ -92,7 +99,7 @@ class MaslouRLModel2QDiscrete(ABC):
                     self.env.render()
                     break
             print(
-                f"episode {episode} finished in {step+1} steps with reward {episode_reward:.2f}. "
+                f"episode {episode} finished in {step + 1} steps with reward {episode_reward:.2f}. "
                 f"And took: {(time() - episode_start_time):.2f} seconds. ")
             rewards.append(episode_reward)
         print("Average reward ", np.mean(rewards))
@@ -100,10 +107,12 @@ class MaslouRLModel2QDiscrete(ABC):
     def remember(self, state, action, reward, state_, done):
         self.memory.store_transition(state, action, reward, state_, done)
 
-    def choose_action(self, state, test=False):
-        epsilon = 0 if test else self.epsilon
+    def choose_action(self, state):
         q_values = get_q_values(self.Q_eval, state)
-        action = select_action_epsilon_greedy(q_values, epsilon)
+        if self.train_state:
+            action = select_action_epsilon_greedy(q_values, self.epsilon)
+        else:
+            action = select_best_action(q_values)
         return action
 
     def learn(self, batch_size, discount_factor):
@@ -120,7 +129,7 @@ class MaslouRLModel2QDiscrete(ABC):
             q_y = q_pred[:]
             batch_indices = np.arange(batch_size, dtype=np.int32)
             q_y[batch_indices, action_indices] = reward + discount_factor * \
-                                                 q_target[batch_indices, max_actions.astype(int)] * (1-done)
+                                                 q_target[batch_indices, max_actions.astype(int)] * (1 - done)
             _ = self.Q_eval.fit(state, q_y, verbose=0)
 
             self.epsilon = self.epsilon - self.epsilon_decay if self.epsilon > self.epsilon_min else self.epsilon_min
@@ -131,12 +140,13 @@ class MaslouRLModel2QDiscrete(ABC):
     def update_target_model(self):
         self.Q_target.set_weights(self.Q_eval.get_weights())
 
-    def load_model(self, model_file, prepare_target=False):
+    def load_model(self, model_file):
         path = model_file.split(".")
         path_without_ext = ".".join(path[:-1])
         self.Q_eval = load_model(path_without_ext + "." + path[-1])
-        if prepare_target:
+        if self.train_state:
             self.Q_target = load_model(path_without_ext + "_t." + path[-1])
+            self.memory = ReplayBuffer.load(path_without_ext + "_memory.npz")
 
     @abstractmethod
     def build_model(self):
@@ -146,23 +156,30 @@ class MaslouRLModel2QDiscrete(ABC):
         path = model_file.split(".")
         path_without_ext = ".".join(path[:-1])
         self.Q_eval.save(path_without_ext + "." + path[-1])
-        self.Q_target.save(path_without_ext + "_t." + path[-1])
+        if self.train_state:
+            self.Q_target.save(path_without_ext + "_t." + path[-1])
+            self.memory.save(path_without_ext + "_memory.npz")
 
 
 class MaslouRLModelDDPGContinuous(ABC):
-    def __init__(self, env, replay_buffer_size=10000):
+    def __init__(self, env, replay_buffer_size=10000, train_state=True, need_to_initiate_memory=True):
         self.env = env
         self.input_shape = env.observation_space.shape
         self.n_actions = np.prod(env.action_space.shape)
         self.min_actions = env.action_space.low
         self.max_actions = env.action_space.high
-        self.memory = ReplayBuffer(replay_buffer_size, input_shape=self.input_shape, n_actions=self.n_actions,
-                                   discrete=False)
+        self.train_state = train_state
+        self.memory = None
+        if self.train_state and need_to_initiate_memory:
+            self.memory = ReplayBuffer(replay_buffer_size, input_shape=self.input_shape, n_actions=self.n_actions,
+                                       discrete=False)
         self.actor = self.build_actor_model()
-        self.actor_target = self.build_actor_model()
         self.critic = self.build_critic_model()
-        self.critic_target = self.build_critic_model()
-        self.update_target_models(tau=1)
+
+        if self.train_state:
+            self.actor_target = self.build_actor_model()
+            self.critic_target = self.build_critic_model()
+            self.update_target_models(tau=1)
 
     def summary(self):
         print("Observation space:", self.env.observation_space)
@@ -184,19 +201,25 @@ class MaslouRLModelDDPGContinuous(ABC):
         self.actor.save(path_without_ext + "_actor." + path[-1])
         self.critic.save(path_without_ext + "_critic." + path[-1])
 
-        self.actor_target.save(path_without_ext + "_actor_t." + path[-1])
-        self.critic_target.save(path_without_ext + "_critic_t." + path[-1])
+        if self.train_state:
+            self.actor_target.save(path_without_ext + "_actor_t." + path[-1])
+            self.critic_target.save(path_without_ext + "_critic_t." + path[-1])
+            self.memory.save(path_without_ext + "_memory.npz")
 
-    def load_model(self, model_file, prepare_target=False):
-        actor_model_path = model_file + "_actor.h5"
-        critic_model_path = model_file + "_critic.h5"
-        actor_t_model_path = model_file + "_actor_t.h5"
-        critic_t_model_path = model_file + "_critic_t.h5"
+    def load_model(self, model_file):
+        path = model_file.split(".")
+        path_without_ext = ".".join(path[:-1])
+        actor_model_path = path_without_ext + "_actor.h5"
+        critic_model_path = path_without_ext + "_critic.h5"
+        actor_t_model_path = path_without_ext + "_actor_t.h5"
+        critic_t_model_path = path_without_ext + "_critic_t.h5"
+        memory_path = path_without_ext+"_memory.npz"
         self.actor = load_model(actor_model_path)
         self.critic = load_model(critic_model_path)
-        if prepare_target:
+        if self.train_state:
             self.actor_target = load_model(actor_t_model_path)
             self.critic_target = load_model(critic_t_model_path)
+            self.memory = ReplayBuffer.load(memory_path)
 
     def update_target_models(self, tau):
         weights = []
@@ -214,10 +237,10 @@ class MaslouRLModelDDPGContinuous(ABC):
     def remember(self, state, action, reward, state_, done):
         self.memory.store_transition(state, action, reward, state_, done)
 
-    def choose_action(self, state, noise=None, test=False):
+    def choose_action(self, state, noise=None):
         state = state.reshape((-1, *self.input_shape))
         actions = self.actor.predict(state, verbose=0)
-        if not test:
+        if self.train_state:
             actions += np.random.normal(0, noise, self.n_actions)
             actions = np.clip(actions, self.min_actions, self.max_actions)
         return actions
@@ -236,7 +259,7 @@ class MaslouRLModelDDPGContinuous(ABC):
             target_actions = self.actor_target(states_)
             critic_values_ = tf.squeeze(self.critic_target([states_, target_actions]), axis=1)
             critic_values = tf.squeeze(self.critic([states, actions]), axis=1)
-            target = rewards + discount_factor * (1-done) * critic_values_
+            target = rewards + discount_factor * (1 - done) * critic_values_
             critic_loss = tensorflow.keras.losses.MSE(target, critic_values)
             critic_network_gradient = tape.gradient(critic_loss, self.critic.trainable_variables)
             self.critic.optimizer.apply_gradients(zip(critic_network_gradient, self.critic.trainable_variables))
@@ -253,6 +276,8 @@ class MaslouRLModelDDPGContinuous(ABC):
               training_batch_size=64, discount_factor=0.99,
               model_backup_frequency_episodes=100, path_to_back_up="./",
               episodes_for_average_tracking=100, file_logger=None):
+        if not self.train_state:
+            raise ValueError("Agent is not in train state")
         self.memory.clear()
         average_tracker = AverageRewardTracker(episodes_for_average_tracking)
         for episode in range(episodes):
@@ -280,7 +305,7 @@ class MaslouRLModelDDPGContinuous(ABC):
                 f"Average reward over last {episodes_for_average_tracking}: {average:.2f} "
                 f"And took: {(time() - episode_start_time):.2f} seconds. ")
             if file_logger is not None:
-                file_logger.log(episode, step+1, episode_reward, average, 0)
+                file_logger.log(episode, step + 1, episode_reward, average, 0)
             if episode != 0 and episode % model_backup_frequency_episodes == 0:
                 backup_file = path_to_back_up + f"model_{episode}.h5"
                 print(f"Backing up model to {backup_file}")
@@ -288,6 +313,8 @@ class MaslouRLModelDDPGContinuous(ABC):
 
     def test(self, episodes, max_steps_per_episode=1000, visualize=False):
         rewards = []
+        if self.train_state:
+            raise ValueError("Agent is in train state")
         for episode in range(episodes):
             print(f"Starting episode {episode}")
             episode_reward = 0
@@ -296,7 +323,7 @@ class MaslouRLModelDDPGContinuous(ABC):
             for step in range(max_steps_per_episode):
                 if visualize:
                     self.env.render()
-                action = self.choose_action(state, test=True)
+                action = self.choose_action(state)
                 action = np.squeeze(action)
                 state, reward, done, info = self.env.step(action)
                 episode_reward += reward
