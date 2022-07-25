769444f66710d7a7cce03a1321fa4df65b3b991a
masalskyi <masalskyi@stud.onu.edu.ua>
2022-07-26 02:53:28 +0300

    .

--- box2d/CarRacing/plot_logging.py
+++ box2d/CarRacing/plot_logging.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 data = pd.read_csv("./logging/log1.csv", sep=';')
-plt.figure(figsize=(20, 12))
-
-plt.plot(data['average'])
-plt.plot(data['reward'])
+plt.figure(figsize=(10, 6))
+plt.plot([800] * len(data['average']), c="gray")
+plt.plot(data['average'], c="blue")
+plt.plot(data['reward'], c="orange")
 plt.xlabel('Episode')
 plt.ylabel('Reward')
-plt.legend(['Average reward', 'Reward'], loc='upper right')
+plt.legend(['Good perfomance', 'Average reward', 'Reward'], loc='upper right')
 plt.title('Reward')
 plt.show()
