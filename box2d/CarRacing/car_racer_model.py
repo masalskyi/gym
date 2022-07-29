654b3a7cd832a0df38c3c2c2cb3cd1c4afbae8c9
masalskyi <masalskyi@stud.onu.edu.ua>
2022-07-29 21:11:51 +0300

    prepare to ppo

--- box2d/CarRacing/car_racer_model.py
+++ box2d/CarRacing/car_racer_model.py
@@ -8,29 +8,28 @@ import tensorflow.keras.layers as layers
 class CarRacerModel(MaslouRLModelDDPGContinuous):
     def build_critic_model(self):
         state_input = layers.Input(shape=self.input_shape)
-        x = layers.Conv2D(filters=32, kernel_size=8, strides=(4, 4), activation="relu", data_format='channels_first')(state_input)
-        x = layers.Conv2D(filters=64, kernel_size=4, strides=(2, 2), activation="relu", data_format='channels_first')(x)
-        x = layers.Conv2D(filters=64, kernel_size=3, strides=(1, 1), activation="relu", data_format='channels_first')(x)
+        x = layers.Conv2D(filters=16, kernel_size=5, strides=(4, 4), activation="relu", data_format='channels_first')(state_input)
+        x = layers.Conv2D(filters=32, kernel_size=3, strides=(3, 3), activation="relu", data_format='channels_first')(x)
+        x = layers.Conv2D(filters=32, kernel_size=3, strides=(1, 1), activation="relu", data_format='channels_first')(x)
         x = layers.Flatten()(x)
 
         action_input = layers.Input(shape=(self.n_actions,))
         x = layers.Concatenate()([x, action_input])
-        x = layers.Dense(512, activation="relu")(x)
         x = layers.Dense(128, activation="relu")(x)
+        x = layers.Dense(64, activation="relu")(x)
         outputs = layers.Dense(1, activation="linear")(x)
         model = Model(inputs=[state_input, action_input], outputs=outputs, name="CarRacer_critic")
-        model.compile(optimizer=Adam(learning_rate=0.0002), loss="mse")
+        model.compile(optimizer=Adam(learning_rate=0.002), loss="mse")
         return model
 
     def build_actor_model(self):
         state_input = layers.Input(shape=self.input_shape)
-        x = layers.Conv2D(filters=32, kernel_size=8, strides=(4, 4), activation="relu", data_format='channels_first')(state_input)
-        x = layers.Conv2D(filters=64, kernel_size=4, strides=(2, 2), activation="relu", data_format='channels_first')(x)
-        x = layers.Conv2D(filters=64, kernel_size=3, strides=(1, 1), activation="relu", data_format='channels_first')(x)
+        x = layers.Conv2D(filters=16, kernel_size=5, strides=(4, 4), activation="relu", data_format='channels_first')(state_input)
+        x = layers.Conv2D(filters=32, kernel_size=3, strides=(3, 3), activation="relu", data_format='channels_first')(x)
+        x = layers.Conv2D(filters=32, kernel_size=3, strides=(1, 1), activation="relu", data_format='channels_first')(x)
         x = layers.Flatten()(x)
-        x = layers.Dense(512, activation="relu")(x)
         x = layers.Dense(128, activation="relu")(x)
         output = layers.Dense(self.n_actions, activation="tanh")(x)
         model = Model(inputs=state_input, outputs=output, name="CarRacer_actor")
-        model.compile(optimizer=Adam(learning_rate=0.00005), loss="mse")
+        model.compile(optimizer=Adam(learning_rate=0.00001), loss="mse")
         return model
