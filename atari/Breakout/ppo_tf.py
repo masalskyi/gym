4058ac2b79ff61f9f45fe3669c1e14edcb2138d5
masalskyi <masalskyi@stud.onu.edu.ua>
2022-08-03 01:52:03 +0300

    maybe working ppo tf

--- atari/Breakout/ppo_tf.py
+++ atari/Breakout/ppo_tf.py
@@ -10,51 +10,48 @@ import numpy as np
 import os
 import gym
 
+
 class MaslouRLModelPPODiscrete(ABC):
     def __init__(self):
         self.env = self.make_env(0, 0, False, 0, "temp")()
         self.input_shape = self.env.observation_space.shape
         self.n_actions = self.env.action_space.n
 
-        self.feature_extractor = self.build_feature_extractor()
-        self.actor = self.build_actor()
-        self.critic = self.build_critic()
+        self.model = self.build_model()
 
     def summary(self):
         print("Observation space:", self.env.observation_space)
         print("Action space:", self.env.action_space)
-        self.actor.summary()
-        self.critic.summary()
-        self.feature_extractor.summary()
+        self.model.summary()
+
 
     @abstractmethod
     def make_env(self, seed, idx, capture_video, capture_every_n_video, run_name):
         pass
 
     @abstractmethod
-    def build_actor(self) -> keras.Model:
-        pass
-
-    @abstractmethod
-    def build_feature_extractor(self) -> keras.Model:
-        pass
-
-    @abstractmethod
-    def build_critic(self) -> keras.Model:
+    def build_model(self) -> keras.Model:
         pass
 
     def get_action_and_value(self, x, action=None):
-        hidden = self.feature_extractor.predict(x, verbose=0)
-        probs = self.actor.predict(hidden, verbose=0)
+        model_res = self.model(x)
+        probs = model_res[0]
         distribution = Categorical(probs=probs)
         if action is None:
             action = distribution.sample()
-        return action, distribution.log_prob(action), distribution.entropy(), self.critic.predict(hidden, verbose=0)
+        return action, distribution.log_prob(action), distribution.entropy(), model_res[1]
+
+    def get_value(self, x):
+        model_res = self.model(x)
+        return model_res[1]
 
     def train(self, learning_rate=2.5e-4, num_steps=128,
               num_envs=4, seed=42, capture_video=True,
               capture_every_n_video=50, run_name="PPO_run_name",
-              total_timesteps=1000000, anneal_lr=True):
+              total_timesteps=1000000, anneal_lr=True, gae=True,
+              discount_gamma=0.99, gae_lambda=0.95, update_epochs=4,
+              minibatches=4, norm_adv=True, clip_coef=0.2, clip_vloss=True,
+              ent_coef=0.01, vf_coef=0.5):
         batch_size = num_steps * num_envs
         envs = gym.vector.SyncVectorEnv([self.make_env(seed + i,
                                                        i,
@@ -67,7 +64,7 @@ class MaslouRLModelPPODiscrete(ABC):
         tf.random.set_seed(seed)
         os.environ["TF_CUDNN_DETERMINISTIC"] = '1'
 
-        optimizer = Adam(lr=learning_rate, epsilon=1e-5)
+        optimizer = Adam(learning_rate=learning_rate, epsilon=1e-5)
         obs = np.zeros((num_steps, num_envs) + envs.single_observation_space.shape, dtype=float)
         actions = np.zeros((num_steps, num_envs) + envs.single_action_space.shape, dtype=float)
         logprobs = np.zeros((num_steps, num_envs), dtype=float)
@@ -93,14 +90,84 @@ class MaslouRLModelPPODiscrete(ABC):
                 dones[step] = next_done
 
                 action, logprob, entropy, value = self.get_action_and_value(next_obs)
-                values[step] = value.flatten()
+                values[step] = tf.squeeze(value)
                 actions[step] = action
                 logprobs[step] = logprob
 
                 next_obs, reward, done, info = envs.step(action)
                 rewards[step] = reward
-                print(info)
-                # for item in info:
-                #     if "episode" in item.keys():
-                #         print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
-                #         break
+                if "episode" in info.keys():
+                    for item in info["episode"]:
+                        if item is not None:
+                            print(f"global_step={global_step}, episodic_return={item['r']}")
+                            break
+
+            next_value = self.get_value(next_obs).numpy().reshape(1,-1)
+            if gae:
+                advantages = np.zeros_like(rewards)
+                lastgaelam = 0
+                for t in reversed(range(num_steps)):
+                    if t == num_steps - 1:
+                        nextnonterminal = 1.0 - next_done
+                        nextvalues = next_value
+                    else:
+                        nextnonterminal = 1.0 - dones[t+1]
+                        nextvalues = values[t+1]
+                    delta = rewards[t] + discount_gamma * nextvalues * nextnonterminal - values[t]
+                    advantages[t] = lastgaelam = delta + discount_gamma * gae_lambda * nextnonterminal * lastgaelam
+                returns = advantages + values
+            else:
+                returns = np.zeros_like(rewards)
+                for t in reversed(range(num_steps)):
+                    if t == num_steps - 1:
+                        nextnonterminal = 1.0 - next_done
+                        next_return = next_value
+                    else:
+                        nextnonterminal = 1.0 - dones[t+1]
+                        next_return = returns[t+1]
+                    returns[t] = rewards[t] + discount_gamma * nextnonterminal * next_return
+                advantages = returns - values
+
+            b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
+            b_logprobs = logprobs.reshape(-1)
+            b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
+            b_advanatges = advantages.reshape(-1)
+            b_returns = returns.reshape(-1)
+            b_values = values.reshape(-1)
+
+            b_inds = np.arange(batch_size)
+            minibatch_size = batch_size // minibatches
+            for epoch in range(update_epochs):
+                np.random.shuffle(b_inds)
+                for start in range(0, batch_size, minibatch_size):
+                    end = start + minibatch_size
+                    mb_inds = b_inds[start:end]
+                    with tf.GradientTape() as tape:
+                        _, newlogprob, entropy, newvalue = self.get_action_and_value(b_obs[mb_inds], b_actions[mb_inds])
+
+                        log_ratio = newlogprob - b_logprobs[mb_inds]
+                        ratio = np.exp(log_ratio)
+
+                        mb_advantages = b_advanatges[mb_inds]
+                        if norm_adv:
+                            mb_advantages = (mb_advantages - np.mean(mb_advantages)) / (np.std(mb_advantages) + 1e-8)
+
+                        pg_loss1 = -mb_advantages * ratio
+                        pg_loss2 = -mb_advantages * tf.clip_by_value(ratio, 1 - clip_coef, 1 + clip_coef)
+                        pg_loss = tf.math.reduce_mean(tf.maximum(pg_loss1, pg_loss2))
+                        newvalue = tf.squeeze(newvalue)
+                        if clip_vloss:
+                            v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
+                            v_clipped = b_values[mb_inds] + tf.clip_by_value(newvalue - b_returns[mb_inds], -clip_coef, clip_coef)
+                            v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
+                            v_loss_max = tf.maximum(v_loss_unclipped, v_loss_clipped)
+                            v_loss = 0.5 * tf.math.reduce_mean(v_loss_max)
+                        else:
+                            v_loss = 0.5 * tf.math.reduce_mean((newvalue - b_returns[mb_inds])**2)
+
+                        entropy_loss = tf.math.reduce_mean(entropy)
+                        loss = tf.cast(pg_loss, dtype=tf.float32) - ent_coef * tf.cast(entropy_loss, dtype=tf.float32) + \
+                               tf.cast(v_loss, dtype=tf.float32) * vf_coef
+                        feature_network_gradient = tape.gradient(loss, self.model.trainable_variables)
+                        optimizer.apply_gradients(zip(feature_network_gradient, self.model.trainable_variables))
+
--- atari/Breakout/test_1.py
+++ atari/Breakout/test_1.py
@@ -1,9 +1,11 @@
 import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
 from tensorflow.keras.initializers import Orthogonal
+from tensorflow.keras.optimizers import Adam
 from ppo_tf import MaslouRLModelPPODiscrete
 import gym
 import numpy as np
+from garage.envs.wrappers.episodic_life import EpisodicLife
 class BreakoutAgent(MaslouRLModelPPODiscrete):
     def __init__(self):
         self.feature_size = 512
@@ -12,6 +14,8 @@ class BreakoutAgent(MaslouRLModelPPODiscrete):
     def make_env(self, seed, idx, capture_video, capture_every_n_episode, run_name):
         def thunk():
             env = gym.make("BreakoutNoFrameskip-v4")
+            env = EpisodicLife(env)
+            env = gym.wrappers.RecordEpisodeStatistics(env)
             env = gym.wrappers.ResizeObservation(env, (84, 84))
             env = gym.wrappers.GrayScaleObservation(env)
             env = gym.wrappers.FrameStack(env, 4)
@@ -21,27 +25,18 @@ class BreakoutAgent(MaslouRLModelPPODiscrete):
             return env
         return thunk
 
-    def build_feature_extractor(self) -> keras.Model:
+    def build_model(self) -> keras.Model:
         input = layers.Input(shape=(4, 84, 84))
         x = layers.Conv2D(filters=32, kernel_size=8, strides=4, activation="relu", kernel_initializer=Orthogonal(np.sqrt(2)), data_format='channels_first')(input)
         x = layers.Conv2D(filters=64, kernel_size=4, strides=2, activation="relu", kernel_initializer=Orthogonal(np.sqrt(2)), data_format='channels_first')(x)
         x = layers.Conv2D(filters=64, kernel_size=3, strides=1, activation="relu", kernel_initializer=Orthogonal(np.sqrt(2)), data_format='channels_first')(x)
         x = layers.Flatten()(x)
-        output = layers.Dense(units=self.feature_size, activation="relu", kernel_initializer=Orthogonal(np.sqrt(2))) (x)
-        model = keras.Model(inputs=input, outputs=output, name="FeatureExtractor")
+        hidden = layers.Dense(units=self.feature_size, activation="relu", kernel_initializer=Orthogonal(np.sqrt(2)))(x)
+        actions = layers.Dense(self.n_actions, activation="softmax", kernel_initializer=Orthogonal(0.01))(hidden)
+        value = layers.Dense(1, activation="linear", kernel_initializer=Orthogonal(1))(hidden)
+        model = keras.Model(inputs=input, outputs=[actions, value], name="BreakoutModel")
         return model
 
-    def build_actor(self) -> keras.Model:
-        input = layers.Input(shape=(self.feature_size,))
-        output = layers.Dense(self.n_actions, activation="softmax", kernel_initializer=Orthogonal(0.01))(input)
-        model = keras.Model(inputs=input, outputs=output, name="Actor")
-        return model
-
-    def build_critic(self) -> keras.Model:
-        input = layers.Input(shape=(self.feature_size,))
-        output = layers.Dense(1, activation="linear", kernel_initializer=Orthogonal(1))(input)
-        model = keras.Model(inputs=input, outputs=output, name="Critic")
-        return model
 
 agent = BreakoutAgent()
 agent.summary()
